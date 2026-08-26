510c38c8989bb712a9fd1f9e4aa496c9485d12cb
Creator-reappear <creatorreappear@gmail.com>
2026-08-26 20:22:15 +0530

    Add function to calculate temperature range
    1. Accepts space-separated integers representing temperature readings.
    2.Finds the highest and lowest temperature recorded.
    3.Calculates and prints the temperature range, defined as the difference between the highest and lowest readings.
    4.If there is only one reading, print 0.

--- Week-04/3_Assignments/assignment_01.py
+++ Week-04/3_Assignments/assignment_01.py
@@ -1,1 +1,15 @@
+def calculate_temperature_range():
+  user_input = input()
 
+  readings = [int(x) for x in user_input.split()]
+
+  if len(readings) <= 1:
+    print(0)
+  else:
+    highest = max(readings)
+    lowest = min(readings)
+    temp_range = highest - lowest
+    print(temp_range)
+
+
+calculate_temperature_range()
