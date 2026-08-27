7f1981dd7ec0de5ec7e893872788a300af7c8ec4
Creator-reappear <creatorreappear@gmail.com>
2026-08-27 22:29:46 +0530

    Add function to identify elements with two occurrences
    (a) Accepts a space-separated sequence of integers.
    (b) Returns all numbers that occur exactly two times.
    (c) The numbers should be returned in increasing order.
    (d) If no number occurs exactly two times, returns -1.

--- Week-05/Assignments/assignment_03.py
+++ Week-05/Assignments/assignment_03.py
@@ -1,1 +1,25 @@
+def exactly_two(L):
+    a=[int(float(x)) for x in L]
 
+    b=[]
+    for x in range(len(a)):
+        count=0
+        for y in range(len(a)):
+            if(a[y]==a[x]):
+                count=count+1
+        if count==2:
+            b.append(a[x])
+
+    d=[]
+    for x in b:
+        if x not in d:
+            d.append(x)
+
+    d.sort()
+    if(len(b)==0):
+        print("-1")
+    else:
+        print(*d)
+
+L=input().split()
+exactly_two(L)
