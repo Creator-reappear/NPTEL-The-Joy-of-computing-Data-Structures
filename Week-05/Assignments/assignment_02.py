3555a79350fd073bc5749a326f87f7acd6e15b3a
Creator-reappear <creatorreappear@gmail.com>
2026-08-27 22:28:53 +0530

    Add greater_one function to identify duplicates
    (a) Accepts a space-separated sequence of integers.
    (b) Returns the number that occurs most frequently.
    (c) If two or more numbers have the same highest frequency, returns the smallest number.

--- Week-05/Assignments/assignment_02.py
+++ Week-05/Assignments/assignment_02.py
@@ -1,1 +1,23 @@
+def greater_one(L):
+    a=[int(float(x)) for x in L]
 
+    b=[]
+    for x in range(len(a)):
+        count=0
+        for y in range(len(a)):
+            if(a[y]==a[x]):
+                count=count+1
+        if count>1:
+            b.append(a[x])
+
+    d=[]
+    for x in b:
+        if x not in d:
+            d.append(x)
+
+    if(len(b)==0):
+        print("-1")
+    else:print(*d)
+
+L=input().split()
+greater_one(L)
