32beac66ff7d0596e1f7af172f51e4cb4e3d2929
Creator-reappear <creatorreappear@gmail.com>
2026-08-27 22:08:20 +0530

    Implement frequency count and find min of max freq
    (a) Accepts a space-separated sequence of integers.
    (b) Returns the number that occurs most frequently.
    (c) If two or more numbers have the same highest frequency, returns the smallest number.

--- Week-05/Assignments/assignment_01.py
+++ Week-05/Assignments/assignment_01.py
@@ -1,1 +1,19 @@
+L=input().split()
+a=[int(float(x)) for x in L]
 
+b=[]
+for x in range(len(a)):
+    count=0
+    for y in range(len(a)):
+        if(a[y]==a[x]):
+            count=count+1
+    b.append(count)
+
+c=max(b)
+d=[]
+for x in range(len(a)):
+    if(b[x]==c):
+        d.append(a[x])
+
+c=min(d)
+print(c)
