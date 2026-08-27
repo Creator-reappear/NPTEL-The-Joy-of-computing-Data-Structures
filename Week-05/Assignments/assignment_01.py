bf8a416392a5671c364d664e37377bdfdda95db7
Creator-reappear <creatorreappear@gmail.com>
2026-08-27 22:35:48 +0530

    Encapsulate frequency logic in frequency function
    Write a function that:
    
    (a) Accepts a space-separated sequence of integers.
    (b) Returns the number that occurs most frequently.
    (c) If two or more numbers have the same highest frequency, returns the smallest number.

--- Week-05/Assignments/assignment_01.py
+++ Week-05/Assignments/assignment_01.py
@@ -1,19 +1,22 @@
+def frequency(L):
+    a=[int(float(x)) for x in L]
+
+    b=[]
+    for x in range(len(a)):
+        count=0
+        for y in range(len(a)):
+            if(a[y]==a[x]):
+                count=count+1
+        b.append(count)
+
+    c=max(b)
+    d=[]
+    for x in range(len(a)):
+        if(b[x]==c):
+            d.append(a[x])
+
+    c=min(d)
+    print(c)
+
 L=input().split()
-a=[int(float(x)) for x in L]
-
-b=[]
-for x in range(len(a)):
-    count=0
-    for y in range(len(a)):
-        if(a[y]==a[x]):
-            count=count+1
-    b.append(count)
-
-c=max(b)
-d=[]
-for x in range(len(a)):
-    if(b[x]==c):
-        d.append(a[x])
-
-c=min(d)
-print(c)
+frequency(L)
