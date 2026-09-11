941d3b98cfc9c8857da16a5756d2fa21f1cc3dbc
Creator-reappear <creatorreappear@gmail.com>
2026-09-11 19:59:11 +0530

    Implement search for element index in list
    Add functionality to find the index of a specified element in a list.

--- Week-07/Assignments/assignment_02.py
+++ Week-07/Assignments/assignment_02.py
@@ -1,1 +1,16 @@
+# 
+N = int(input().strip())
+a=[]
 
+a= input().strip().split()
+found=-1
+b = input().strip()
+
+for i in range(N):
+    if a[i]==b:
+        print(i)
+        found=0
+        break
+
+if found<0:
+    print("-1")
